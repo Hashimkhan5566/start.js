56f678be835b4286b432aa984dfa0b45eb4fea0e
Hashim khan <hashimkhanlazak0301@gmail.com>
2025-09-12 23:47:47 +0500

    then i will start the javascript from basic and we want to expert in jvascript

---
+++ start.js
@@ -0,0 +1,16 @@
+// let name = "hashim_khan";
+// let age= 12;
+// let city = "islamabd";
+// const country = "pakistan";
+
+// console.log(name);
+// console.log(age);
+// console.log(city);
+// console.log(country);
+
+let x = 4;
+let y = 10;
+
+x = x+y;
+console.log(x);
+
