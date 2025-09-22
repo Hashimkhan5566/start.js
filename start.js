b8b30d3df4a1f3b73e1d45ce074f89e5ba4426a4
Hashim khan <hashimkhanlazak0301@gmail.com>
2025-09-22 10:38:40 +0500

    this is the for and while loop example basic

--- start.js
+++ start.js
@@ -1,289 +1,64 @@
-//  let name = "hashim_khan";
-// let age1= 12;
-// let city1 = "islamabd";
-// const country = "pakistan";
-
-// console.log(name);
-// console.log(age);
-// console.log(city);
-// console.log(country);
-
-// let x = 4;
-// let y = 10;
-
-//  y = x+y;
-// console.log(y);
-
-// let obj =  {
-// harry : 12,
-//  was : 34,
-// erra : 56,
-// arre : 89,
-//  errae : 98
-
-// }
-//  for (let a in obj) {
-// console.log(a);
-// }
-
-// let name1  = "hashim";
-// let age = 23;
-// let catogries = "IS Student";
-// let city = "peshawar";
-// console.log(name1);
-// console.log(age);
-// console.log(catogries);
-// console.log(city);
-// console.log(typeof(name1));
-// console.log(typeof(age));
-// console.log(typeof(catogries));
-// console.log(typeof(city));
-// let a = 10;
-// let b = 3;
-//  console.log(a+b);
-// console.log(a*b);
-// console.log(a-b);
-//  console.log(a/b);
-// console.log(a**b);
-//  console.log(a%b);
-//  console.log("ghjfka");
-
-// // // the logic question is term:::
-// // let a1 = 7;
-// // let b1 = 3;
-// // console.log(a1%b1);
-
-// // console.log("1");
-
-// // // 2nd question///
-// // let x = 2;
-// // x = x**3;
-// // console.log(x);
-
-// // console.log("2");
-
-
-
-// // // 3rd question
-
-// // let num = 10;
-// // num +=5;
-// // console.log(num);
-
-// // console.log("3");
-// // // 4th question
-// // let val = 20;
-// // val -=7;
-// // console.log(val);
-
-// // console.log("4");
-// // //5th question
-
-// // let n = 15;
-
-// // console.log(n>10 && n<20);
-
-// // console.log("5");
-
-// // //6th question
-
-// // let check = false;
-// // console.log(!check);
-
-// // console.log("6");
-// // //7th question
-// // let string  = 10;
-// // let number = "10";
-// // console.log(string!=number);
-// // console.log(string!==number);
-
-
-// // console.log("7");
-// // //8th question
-
-// // let c = 4;
-// // c*= 3;
-
-// // console.log(c);
-
-// // console.log("8");
-// // //9th question
-
-// // let agel = 18;
-// // console.log(age>=18? "adult": "minor");
-// // //10 question
-// // let m= 5;
-// // let n2 = 2;
-
-// // console.log(m/n2);
-
-// // console.log("9");
-// // //11 quetsion
-
-// // let text = "hello";
-
-// // text+= "world";
-
-// // console.log(text);
-
-// // console.log("10");
-// // //12 question
-
-// // let num3 = 10;
-// // let num4 = "10";
-
-// // console.log(num3==num4);
-// // console.log(num3===num4);
-
-// // console.log("11");
-// // //13 question
-
-// // let result = (5+2)*3;
-
-// // console.log(result);
-
-// // console.log("12");
-// // //14 question
-
-// // let a3 = 10;
-// // let b4 = 20;
-// // let c5 = 30;
-
-// // console.log(a3<b4&&c5>b4);
-
-// // console.log("13");
-// // //15 question
-// // let isOn = true;
-// // let isOff = false;
-
-// // console.log(isOn||isOff);
-
-// // console.log("14");
-// // //16 quetsion
-
-// // let num8 = 0;
-// // console.log(Boolean(num8));
-
-// // console.log("15");
-// // //17question
-
-// // let value;
-
-// // console.log(typeof value);
-
-// // console.log("16");
-// // //18 question
-
-// // let data = null;
-// // console.log(typeof data);
-
-// // console.log("17");
-// // //19 question
-
-// // let umn = 2;
-// // console.log(umn**4);
-// ///////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\
-// // // javascript daily task
-
-// // //example 1: print the 1 to 50 number 
-
-// // let nomber = prompt("enter the number");
-// // let number2 = parseInt(nomber);
-// // console.log(number2);
-
-// // for (let i=0; i<=50; i++){
-// //     console.log(i);
-// // }
-
-// //2 example: print only even number between 1 to 100
-
-// // for (let i=1; i<=100; i++){
-// //     if(i%2===0){
-// //        console.log(i);
-// //     }
-
-// // }
-
-// //3:print the number 100 all down to number 1
-// // for(i=100; i>=1; i--){
-// //     console.log(i);
-// // }
-
-// //4 print the odd number 
-
-// // for(let i=1; i<=100; i++){
-// //     if(i%2===1){
-// //         console.log(i);
-// //     }
-// // }
-
-
-// //5: print the multiplication 11
-
-// // for(i=1; i<=10; i++){
-// //     console.log(`11X${i}= ${11*i}`);
-// // }
-
-//sum of first 20 natural number
-let sum1 = 0;
-for(let i=0; i<=20; i++){
-    sum1+=i;
-
+// for loop 
+// the 1st question of for loop
+for(let i=0; i<=50; i++){
+    console.log(i);
 }
-console.log("the i is 20 natural number: ", sum1);
 
-//while loop
+//2nd question of for loop
 
-// let i = 0;
-// while(i<=50){
-//     console.log(i);
-//     i++;
-// }
-
-// print odd number
-
-// let i =1;
-// while(i<=50){
-//     if(i%2===1){
-//         console.log(i)
-//     }
-//     i++;
-// }
-
-// Keep adding numbers starting from 1 until the sum becomes greater than 200, then stop.
-
-let sum = 0;
-
-let i=1;
-
-while(sum<=100){
-    sum+=i;
-    i++;
+for(i=0; i<=100; i++){
+    if(i%2==0){
+        console.log(i);
+    }
 }
-console.log("the sum of final", sum);
-console.log("the sum of 20",i-1 );
 
-//reverse the number
+//3rd question of for loop
+//Find the sum of first 20 natural numbers.
 
-let num = 12345;
-let reverse = 0
+let sum =0;
 
-while(num>0){
- let digit = num%10;
- reverse =  reverse*10+digit;
- num= Math.floor(num/10);
+for(let j=0; j<=20; j++){
+  sum+=j;
+  console.log("the sum of total number", sum);
 }
-console.log("the reverse number is :", reverse);
+console.log("the sum of total number is:", sum);
 
+//4th question of for loop
+//Print the multiplication table of 7.
 
-//factorial example 
-
-let number = 11;
-let factorial = 1;
-
-while(number>0){
-  factorial*=number;
-  number--;
+for(k=1; k<=10; k++){
+    console.log(`7X${k}=${7*k}`);
 }
-console.log("the factorial number is:", factorial);
+
+//Print all numbers from 100 down to 1.
+
+for(let i=100; i<=1; i--){
+    console.log(i);
+}
+
+//while loop 1st question
+//Print numbers from 1 to 10 using while loop
+
+let m= 1;
+while(m<=10){                             //(1) if we condition write before the console they print the 2 to 11
+    m++;                                  //(2) if we write the condition after the console they print 1 to 10
+    console.log(m);                       // (3)if we write only once console inside the loop they print only the total
+}                                         // ... number like 11 or 10.
+console.log(m);
+
+// 2nd question
+//Print all odd numbers between 1 and 50.
+
+let n = 0;
+while(n<=50){
+  if(n%2==1){
+    console.log(n);
+  }
+  n++;    
+}
+
+console.log("the total number print");
+console.log(n);
+
+//Keep adding numbers starting from 1 until the sum becomes greater than 200, then stop.
 
